3c19d317b2e3cd647dde25ebc589d3a6c84b5d36
Zreal <zenglz_pro@163.com>
2020-08-13 18:41:17 +0800

    HMM test

---
+++ src/test/HMMtest.py
@@ -0,0 +1,16 @@
+'''
+ * @author Waileinsamkeit
+ * @email Zenglz_pro@163.com
+ * @create date 2020-08-12 11:39:58
+ * @modify date 2020-08-12 11:39:58
+'''
+
+import sys
+import os
+sys.path.append(os.path.join(os.getcwd(),'src'))
+
+from model import HMM
+
+
+if __name__=="__main__":
+    pass
